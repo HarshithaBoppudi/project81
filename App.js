e17b79ae32b585b29dd86930d5271ff3f3d47455
HarshithaBoppudi <67325628+HarshithaBoppudi@users.noreply.github.com>
2021-06-18 08:24:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native'
+import DrawerNavigator from './Navigation/DrawerNavigation';
+
+
+export default function App() {
+  return (
+   <NavigationContainer>
+     <DrawerNavigator/>
+   </NavigationContainer>
+  );
+}
---
+++ Feed.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Feed extends React.Component {
+    render(){
+  return (
+    <View style={{flex:1}}>
+      <Text>FEED SCREEN</Text>
+     
+    </View>
+  );
+  } 
+}
